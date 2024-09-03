67fa1e94b507f489ae00ca8f404e6cd52800ec8a
MBIMBA Triomphe <mbimbatriomphe@gmail.com>
2024-09-03 14:41:37 +0200

    Début video Grafikart "doctrine".

--- TutoSymfony/src/Controller/RecipeController.php
+++ TutoSymfony/src/Controller/RecipeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Repository\RecipeRepository; 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -127,12 +128,13 @@ use Symfony\Component\Routing\Attribute\Route;
 
         //***************************************************** MOTEUR DE TEMPLATE TWIG: partie du fichier "show.html.twig"  *************************************************************
 
-        class RecipeController extends AbstractController
+       /* class RecipeController extends AbstractController
         {
     
             #[Route('/recette', name: 'recipe.index')]
             public function index(Request $request): Response
             {
+             
               #dd($request->attributes->get('slug'), $request->attributes->getInt('id'));        #getIn: permet de donner un entier
             return $this->render('recipe/index.html.twig');
             }
@@ -156,4 +158,38 @@ use Symfony\Component\Routing\Attribute\Route;
             
     
             }
-        }
+        } */
+
+          //*****************************************************L'ORM Doctrine   *************************************************************
+
+          class RecipeController extends AbstractController
+        {
+      
+              #[Route('/recette', name: 'recipe.index')]
+              public function index(Request $request, RecipeRepository $repository): Response
+              {
+                $recipes = $repository->findAll();  // Find prend en paramètre l'id et permet de trouver un enregistrement en particulier. FindBy permet de spécifier un critère sous forme de tableau
+               dd($recipes);
+              return $this->render('recipe/index.html.twig');
+              }
+      
+      
+              #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
+              public function show(Request $request, string $slug, int $id): Response
+              {
+                #dd($request->attributes->get('slug'), $request->attributes->getInt('id'));        #getIn: permet de donner un entier
+      
+              return $this->render('recipe/show.html.twig' , [
+                  'slug' => $slug,
+                  'id' => $id,
+                  'demo' => '<strong>Hello</strong>',
+                  'person' => [
+                      'firstname' => 'john',
+                      'lastname' => 'Doe'
+                  ]
+              ]);
+      
+              
+      
+              }
+          }
