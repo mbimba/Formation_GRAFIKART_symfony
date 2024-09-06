771850646c28bf552b65ec772288472b167bb8c3
MBIMBA Triomphe <mbimbatriomphe@gmail.com>
2024-09-06 10:46:24 +0200

    Suite Video GrafikArt "les formulaires" jusqu'a 10min37.

--- TutoSymfony/src/Form/RecipeType.php
+++ TutoSymfony/src/Form/RecipeType.php
@@ -6,13 +6,26 @@ use App\Entity\Recipe;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;       // Ajout de cet use qui n'apparait pas dans la video de GrafikArt "formulaires": 8min53 pour ajouter le bouton "save" de mon formulaire
+use Symfony\Component\Form\Extension\Core\Type\TextType;         // Ajout de cet use qui n'apparait pas dans la video de GrafikArt "formulaires": 9min23 pour changer le 'title' de mon formulaire par 'titre. 
 
+
+
+/*  ***************************************             *******************************************         */
+ 
+/*
+/*
 class RecipeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
+         /*   ->add('title', TextType::class, [                     // Video: "forumulaires": 9min23: Pour changer le champs 'title'      
+                    'label' => 'Titre'
+            ])       
+        */
+/*
+            ->add('title')                
             ->add('slug')
             ->add('content')
             ->add('createdAt', null, [
@@ -21,7 +34,44 @@ class RecipeType extends AbstractType
             ->add('updatedAt', null, [
                 'widget' => 'single_text'
             ])
-            ->add('duration')
+            ->add('duration')           
+            ->add('save', SubmitType::class, [                     // Video: "forumulaires": 8min53: Ajout d'un nouveau bouton
+                    'label' => 'Envoyer'                            // pour modifier le libellé "save" du dessus, j'ai ajouté ce label poue "envoyer"
+            ])     
+                
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Recipe::class,
+        ]);
+    }
+}       */
+
+/*  ***************************************  VIDEO GrafikArt   "LES FORMULAIRES":         *******************************************         */
+
+class RecipeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        
+            ->add('title')                
+            ->add('slug')
+            ->add('content')
+            ->add('createdAt', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('duration')           
+            ->add('save', SubmitType::class, [                     // Video: "forumulaires": 8min53: Ajout d'un nouveau bouton
+                    'label' => 'Envoyer'                            // pour modifier le libellé "save" du dessus, j'ai ajouté ce label poue "envoyer"
+            ])     
+                
         ;
     }
 
